1ec32c854a95ea281d77084a28124653192434f7
Pankaj Bhatia <panbhati@adobe.com>
2024-07-01 02:07:57 +0530

    updating styles

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,21 +6,22 @@ function Home() {
 
     useEffect(() => {
     (async () => {
-      console.log("inside effect")
       const res = await fetch('http://13.201.129.179/getQuote')
       const data = await res.json()
       setRandomQuote(data.quote)
-      console.log(data.quote)
     })()
 
   }, [])
 
   return (
-    <div>
+    <div className="h-screen bg-slate-900">
+        <div className="h-1/6 text-center pt-5">
+        <h1 className='text-3xl font-bold h-11 text-white '> Daily dose of dad-astic humour </h1>
+        </div>
+        <div className="h-5/6 font-bold flex pb-10 justify-center items-center">
+        <div className="pb-72  text-white antialiased">{randomQuote}</div> 
+        </div>
 
-
- <h1 className='text-3xl font-bold underline'> Quote of the day </h1> 
-      <div>{randomQuote}</div> 
     </div>
   )
 }
